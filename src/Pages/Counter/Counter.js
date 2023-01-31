6ef3f6178b1178f1d471503e2a05062023b7ed14
Rubayet-billah <rubayetbillah909@gmail.com>
2023-01-31 18:15:05 +0600

    styled

--- src/Pages/Counter/Counter.js
+++ src/Pages/Counter/Counter.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Counter = () => {
+    const [count, setCount] = useState(0)
     return (
-        <div>
-            <div className='bg-green-200 text-center'>
-                Count:
+        <div className='flex justify-center mt-24'>
+            <div className='bg-green-200 text-center p-6 rounded-md'>
+                <h2 className='text-2xl'>Count: {count}</h2>
+                <button onClick={() => setCount(count + 1)} className='bg-gradient-to-r from-red-500 to-pink-400 px-5 py-2 m-4 rounded-md text-white'>Increment</button>
+                <button onClick={() => setCount(prvState => prvState - 1)} className='bg-gradient-to-r from-blue-500 to-violet-400 px-5 py-2 m-4 rounded-md text-white'>Decrement</button>
             </div>
         </div>
     )
