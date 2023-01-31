28da4923541d09f815b3e4a7ab0c5817b474f419
Rubayet-billah <rubayetbillah909@gmail.com>
2023-01-31 22:57:56 +0600

    form added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Pages/Counter/Counter';
+import Form from './Pages/Form/Form';
 
 function App() {
   return (
     <div>
       <h1 className='text-4xl text-center font-bold mt-12'>Counter App</h1>
-      <Counter />
+      {/* <Counter /> */}
+      <Form />
     </div>
   );
 }
--- src/Pages/Counter/Counter.js
+++ src/Pages/Counter/Counter.js
@@ -5,22 +5,22 @@ const Counter = () => {
 
     const reducer = (state, action) => {
         if (action.type === "INCREMENT") {
-            return state + 1;
+            return state + action.payload.count;
         } else if (action.type === "DECREMENT") {
-            return state - 1;
+            return state - action.payload.count;
         }
     }
 
     const [state, dispatch] = useReducer(reducer, initialState);
     return (
-        <div className='flex justify-center mt-24'>
+        <div className='flex justify-center mt-8'>
             <div className='bg-green-200 text-center p-6 rounded-md'>
                 <h2 className='text-2xl'>Count: {state}</h2>
                 <button
-                    onClick={() => dispatch({ type: "DECREMENT" })}
+                    onClick={() => dispatch({ type: "DECREMENT", payload: { count: 4 } })}
                     className='bg-gradient-to-r from-blue-500 to-violet-400 px-5 py-2 m-4 rounded-md text-white'>Decrement</button>
                 <button
-                    onClick={() => dispatch({ type: "INCREMENT" })}
+                    onClick={() => dispatch({ type: "INCREMENT", payload: { count: 5 } })}
                     className='bg-gradient-to-r from-red-500 to-pink-400 px-5 py-2 m-4 rounded-md text-white'>Increment</button>
             </div>
         </div>
