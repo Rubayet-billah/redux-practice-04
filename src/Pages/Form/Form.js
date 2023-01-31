9d358ec8510b5311bf1d5bdfd534bc5b06c1d6c7
Rubayet-billah <rubayetbillah909@gmail.com>
2023-02-01 01:46:51 +0600

    target

--- src/Pages/Form/Form.js
+++ src/Pages/Form/Form.js
@@ -1,49 +1,130 @@
-import React, { useReducer } from 'react';
+import React, { useReducer } from "react";
 
 const Form = () => {
+  const initialState = {
+    email: "",
+    password: "",
+    confirmPassword: "",
+    term: true,
+  };
 
+  const reducer = (state, action) => {
+    console.log(state);
+    switch (action.type) {
+      case "INPUT":
+        return {
+          ...state,
+          [action.payload.name]: action.payload.value,
+        };
 
-    const [] = useReducer()
-
-    const submitData = (event) => {
-        event.preventDefault()
+      default:
+        return state;
     }
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    return (
-        <div className='flex justify-center'>
+  const submitData = (event) => {
+    event.preventDefault();
+    console.log(state);
+  };
 
-            <form onSubmit={() => submitData()} className='w-3/4'>
-                <div className="mb-6">
-                    <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                    <input type="email" id="email" name='email' className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
-                </div>
-                <div className="mb-6">
-                    <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                    <input type="password" id="password" name='password' className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
-                </div>
-                <div className="mb-6">
-                    <label for="repeat-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Repeat password</label>
-                    <input type="password" id="repeat-password" name='confirmPassword' className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
-                </div>
-                <div className="mb-6">
-                    <button
-
-                        className='bg-gradient-to-r from-blue-500 to-violet-400 px-5 py-2 m-4 ml-0 rounded-md text-white'>-</button>
-                    <span className='text-2xl'>Count: 0</span>
-                    <button
-
-                        className='bg-gradient-to-r from-red-500 to-pink-400 px-5 py-2 m-4 rounded-md text-white'>+</button>
-                </div>
-                <div className="flex items-start mb-6">
-                    <div className="flex items-center h-5">
-                        <input id="terms" type="checkbox" value="" name='checkbox' className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
-                    </div>
-                    <label for="terms" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="/" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a></label>
-                </div>
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
-            </form>
+  return (
+    <div className="flex justify-center">
+      <form onSubmit={() => submitData()} className="w-3/4">
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onBlur={(e) => {
+              dispatch({
+                type: "INPUT",
+                payload: { name: e.target.name, value: e.target.value },
+              });
+            }}
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            placeholder="name@flowbite.com"
+            required
+          />
         </div>
-    );
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="repeat-password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Repeat password
+          </label>
+          <input
+            type="password"
+            id="repeat-password"
+            name="confirmPassword"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <button className="bg-gradient-to-r from-blue-500 to-violet-400 px-5 py-2 m-4 ml-0 rounded-md text-white">
+            -
+          </button>
+          <span className="text-2xl">Count: 0</span>
+          <button className="bg-gradient-to-r from-red-500 to-pink-400 px-5 py-2 m-4 rounded-md text-white">
+            +
+          </button>
+        </div>
+        <div className="flex items-start mb-6">
+          <div className="flex items-center h-5">
+            <input
+              id="terms"
+              type="checkbox"
+              value=""
+              name="terms"
+              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+              required
+            />
+          </div>
+          <label
+            htmlFor="terms"
+            className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            I agree with the{" "}
+            <a
+              href="/"
+              className="text-blue-600 hover:underline dark:text-blue-500"
+            >
+              terms and conditions
+            </a>
+          </label>
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Register new account
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Form;
