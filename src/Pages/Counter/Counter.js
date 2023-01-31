a4da1d0acc59c614da50a082415668fe309ce2f7
Rubayet-billah <rubayetbillah909@gmail.com>
2023-01-31 21:18:16 +0600

    useReducer implemented

--- src/Pages/Counter/Counter.js
+++ src/Pages/Counter/Counter.js
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 const Counter = () => {
-    const [count, setCount] = useState(0)
+    const initialState = 0;
+
+    const reducer = (state, action) => {
+        if (action.type === "INCREMENT") {
+            return state + 1;
+        } else if (action.type === "DECREMENT") {
+            return state - 1;
+        }
+    }
+
+    const [state, dispatch] = useReducer(reducer, initialState);
     return (
         <div className='flex justify-center mt-24'>
             <div className='bg-green-200 text-center p-6 rounded-md'>
-                <h2 className='text-2xl'>Count: {count}</h2>
-                <button onClick={() => setCount(count + 1)} className='bg-gradient-to-r from-red-500 to-pink-400 px-5 py-2 m-4 rounded-md text-white'>Increment</button>
-                <button onClick={() => setCount(prvState => prvState - 1)} className='bg-gradient-to-r from-blue-500 to-violet-400 px-5 py-2 m-4 rounded-md text-white'>Decrement</button>
+                <h2 className='text-2xl'>Count: {state}</h2>
+                <button
+                    onClick={() => dispatch({ type: "DECREMENT" })}
+                    className='bg-gradient-to-r from-blue-500 to-violet-400 px-5 py-2 m-4 rounded-md text-white'>Decrement</button>
+                <button
+                    onClick={() => dispatch({ type: "INCREMENT" })}
+                    className='bg-gradient-to-r from-red-500 to-pink-400 px-5 py-2 m-4 rounded-md text-white'>Increment</button>
             </div>
         </div>
     )
