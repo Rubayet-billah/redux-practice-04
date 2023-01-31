bb95a934ea56e8f39eef4f35385e3deb9ea66d1d
Rubayet-billah <rubayetbillah909@gmail.com>
2023-01-31 12:41:09 +0600

    counter app created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Pages/Counter/Counter';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <h1 className='text-4xl text-center font-bold mt-12'>Counter App</h1>
+      <Counter />
     </div>
   );
 }
---
+++ src/Pages/Counter/Counter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <div>
+            <div className='bg-green-200 text-center'>
+                Count:
+            </div>
+        </div>
+    )
+};
+
+export default Counter;
